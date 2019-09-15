9906fdee6700f9ecfd4fa945056827596b7cc467
sramshet <55179129+sramshet@users.noreply.github.com>
2019-09-15 13:23:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+const mysql = require('mysql');
+
+/**
+ * TODO(developer): specify SQL connection details
+ */
+const connectionName =
+  process.env.INSTANCE_CONNECTION_NAME || 'My Project 95656:us-cental1-a:gcptask9';
+const dbUser = process.env.SQL_USER || 'root';
+const dbPassword = process.env.SQL_PASSWORD || 'Sarika@29';
+const dbName = process.env.SQL_NAME || 'gcptask';
+
+const mysqlConfig = {
+  connectionLimit: 1,
+  user: dbUser,
+  password: dbPassword,
+  database: dbName,
+};
+if (process.env.NODE_ENV === 'production') {
+  mysqlConfig.socketPath = `/cloudsql/${connectionName}`;
+}
+
+// Connection pools reuse connections between invocations,
+// and handle dropped or expired connections automatically.
+let mysqlPool;
+
+exports.mysqlPool = (req, res) => {
+  // Initialize the pool lazily, in case SQL access isn't needed for this
+  // GCF instance. Doing so minimizes the number of active SQL connections,
+  // which helps keep your GCF instances under SQL connection limits.
+  if (!mysqlPool) {
+    mysqlPool = mysql.createPool(mysqlConfig);
+  }
+
+  mysqlPool.query('SELECT NOW() AS now', (err, results) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send(err);
+    } else {
+      res.send(JSON.stringify(results));
+    }
+  });
+  
+
+  // Close any SQL resources that were declared inside this function.
+  // Keep any declared in global scope (e.g. mysqlPool) for later reuse.
+};
